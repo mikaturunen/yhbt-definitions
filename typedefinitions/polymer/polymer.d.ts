0f19920ca28d8fec3420c84bb7df5453619d7a0c
Mika Turunen <mika.turunen@ymail.com>
2015-06-20 11:01:34 +0300

    Updated Polymer to match 1.0 interfacing described in the official documentation.

--- typedefinitions/polymer/polymer.d.ts
+++ typedefinitions/polymer/polymer.d.ts
@@ -1,64 +1,69 @@
-// Type definitions for polymer 
+// Type definitions for Polymer
 // Project: https://github.com/polymer
-// Definitions by: Louis Grignon <https://github.com/lgrignon>
+// Definitions by: Mika Turunen <https://github.com/mikaturunen>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-interface PolymerElement {
-
-	// definition
-	publish?: Object;
-	computed?: Object;
-	// object mapping variable names to functions name
-	observe?: Object;
-
-	// life time API
-	created? (): void;
-	ready? (): void;
-	attached? (): void;
-	domReady? (): void;
-	detached? (): void;
-	attributeChanged? (attrName: string, oldVal: any, newVal: any): void;
-	
-	/**
-	 * User must call from attached callback
-	 */
-	resizableAttachedHandler(): void;
-
-	/**
-	 * User must call from detached callback
-	 */
-	resizableDetachedHandler(): void;
-
-	/**
-	 * User must call from attached callback
-	 */
-	resizerAttachedHandler(): void;
-
-	/**
-	 * User must call from detached callback
-	 */
-	resizerDetachedHandler(): void;
-
-	/**
-	 * User should call when resizing or un-hiding children
-	 */
-	notifyResize(): void;
-}
-
+/**
+ * Interface for Polymer 1.0
+ */
 interface Polymer {
+	importElements(node: Node, callback: Function): void;
 
-    importElements(node: Node, callback: Function): void;
-    import(url: string, callback?: () => void): void;
+	import(url: string, callback?: () => void): void;
 
     mixin(target: any, ...mixins: any[]): any;
-    waitingFor(): Array<string>;
-    // should be an "integer" for milliseconds
+
+	waitingFor(): Array<string>;
+
     forceReady(timeout: number): void;
 
-    (tagName: string, prototype: PolymerElement): void;
-    (tagName: string, prototype: any): void;
-    (prototype: PolymerElement): void;
-    (): void;
+    (elementDescription: PolymerElement): void;
+}
+
+/**
+ * Describes how the element is built.
+ */
+interface PolymerElement {
+	is: string;
+
+	/**
+	 * User defined set of properties for the element. This should reflect the requirements
+	 * the user might have for the Element.
+	 */
+	properties?: any;
+
+	/**
+	 * Called when the element is ready.
+	 */
+	ready?: () => void;
+
+	/**
+	 * Part of lifecycle callbacks: Element created.
+	 */
+	created?: () => void;
+
+	/**
+	 * Part of lifecycle callbacks: Element attached.
+	 */
+	attached?: () => void;
+
+	/**
+	 * Part of lifecycle callbacks: Element detached.
+	 */
+	detached?: () => void;
+
+	/**
+	 * Part of lifecycle callbacks: Elements attributes changed.
+	 * TODO: Find out the typing for "type" argument
+	 */
+	attributeChanged?: (name: string, type: any) => void;
+
+	/**
+	 * List of static attributes on the Host element.
+	 * For example:  hostAttributes: { role: 'button', 'aria-disabled': true, tabindex: 0 }.
+	 * Reflects to:  <x-custom role="button" aria-disabled tabindex="0"></x-custom>.
+	 */
+	hostAttributes?: any;
 }
 
 declare var Polymer: Polymer;
